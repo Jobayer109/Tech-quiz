ea3b0a652347890b9e1c9fe6ac961c294dc1fc11
Nightowlam <nightowl3.00am@gmail.com>
2022-10-11 02:11:04 +0600

    Quiz button set up

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -1,3 +1,4 @@
+import {ChevronRightIcon } from '@heroicons/react/24/solid';
 import React from "react";
 
 const Topic = ({ quiz }) => {
@@ -7,10 +8,14 @@ const Topic = ({ quiz }) => {
       <img className="w-44 h-44 rounded-md border border-purple-400" src={logo} alt="" />
       <div className="text-purple-500 w-[80%] mx-auto">
         <h3 className="font-bold text-2xl">{name}</h3>
-        <p className="font-medium">
-          Questions: <span className="text-white text-lg font semibold">{total}</span>
-        </p>
-        <button className="btn btn-sm btn-outline text-white bg-purple-500 mt-6 w-3/5">Start Quiz</button>
+        <p className="font-medium mt-2">
+          Questions: <span className="text-white text-md font semibold">{total}</span>
+              </p>
+              
+
+              <button className="btn btn-sm btn-outline text-white bg-purple-500 mt-6 w-3/5">
+                  Start Quiz
+                  <ChevronRightIcon className="h-6 w-6 ml-4 text-white"/></button>
       </div>
     </div>
   );
